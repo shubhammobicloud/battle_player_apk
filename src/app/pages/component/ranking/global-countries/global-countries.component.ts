84cb92d4e64080771e095c5ecaa7f0f70cdcbfef
Prasadsanglikar <prasads.mobicloud@gmail.com>
2024-03-27 10:10:17 +0530

    battle-patner-team

--- src/app/pages/component/ranking/global-countries/global-countries.component.ts
+++ src/app/pages/component/ranking/global-countries/global-countries.component.ts
@@ -6,18 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./global-countries.component.scss']
 })
 export class GlobalCountriesComponent {
+  tableHeader = ['Player Name'];
   myTeamList=[
-    {name:'Player 1',score:85},
-    {name:'Player 2',score:75},
-    {name:'Player 3',score:65},
-    {name:'Player 4',score:55},
-    {name:'Player 5',score:45},
-    {name:'Player 6',score:45},
-    {name:'Player 7',score:45},
-    {name:'Player 8',score:45},
-    {name:'Player 9',score:45},
-    {name:'Player 9',score:45},
-    {name:'Player 9',score:45},
-    {name:'Player 9',score:45},
+    { name: 'Player 1', sales: 1, score: 85 },
+    { name: 'Player 2', sales: 1, score: 75 },
+    { name: 'Player 3', sales: 1, score: 65 },
+    { name: 'Player 4', sales: 1, score: 55 },
+    { name: 'Player 5', sales: 1, score: 45 },
+    { name: 'Player 6', sales: 1, score: 45 },
+    { name: 'Player 7', sales: 1, score: 45 },
+    { name: 'Player 8', sales: 1, score: 45 },
+    { name: 'Player 9', sales: 1, score: 45 },
+    { name: 'Player 9', sales: 1, score: 45 },
+    { name: 'Player 9', sales: 1, score: 45 },
+    { name: 'Player 9', sales: 1, score: 45 },
   ]
 }

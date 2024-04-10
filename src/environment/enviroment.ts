264d036059e1a7b71302c11039e10af221ddd0d6
Prasadsanglikar <prasads.mobicloud@gmail.com>
2024-04-10 12:38:57 +0530

    company-unit

--- src/environment/enviroment.ts
+++ src/environment/enviroment.ts
@@ -1,6 +1,10 @@
 export const environment ={
   // baseUrl:'http://89.116.122.208/'
   // baseUrl:'http://localhost:3000/',
+<<<<<<< HEAD
   baseUrl:'http://192.168.29.234:3000/'
+=======
+  baseUrl:'http://192.168.29.234:3000/',
+>>>>>>> 66f389a (company-unit)
   // baseUrl:'http://192.168.29.223:3000/'
 }

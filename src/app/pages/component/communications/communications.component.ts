c4e47ceca1bf6610fee65e7812910516b878cb9e
Prasadsanglikar <prasads.mobicloud@gmail.com>
2024-04-15 18:07:49 +0530

    communications

--- src/app/pages/component/communications/communications.component.ts
+++ src/app/pages/component/communications/communications.component.ts
@@ -1,28 +1,47 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { jwtDecode } from 'jwt-decode';
 
 @Component({
   selector: 'app-communications',
   templateUrl: './communications.component.html',
-  styleUrls: ['./communications.component.scss'],
+  styleUrls: ['./communications.component.scss']
 })
-export class CommunicationsComponent implements OnInit {
-  showTeamChat: boolean = true;
-
+export class CommunicationsComponent  implements OnInit{
+  showTeamChat: boolean = true; // Initially show Team Chat
+  hidesuper:boolean=false
   constructor(private http: HttpClient) {}
-  ngOnInit(): void {
-    const token = localStorage.getItem('token');
 
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  ngOnInit(): void {
+    const token:any = localStorage.getItem('token');
+
+    let data: {
+      _id: any;
+      teamId: any;
+      avatar: any;
+      userName: any;
+      superUser: boolean;
+    } = jwtDecode(token);
+    console.log('check', data.superUser);
+
+    
+    this.hidesuper = data.superUser
+  if(data.superUser){
+
+    this.showTeamChat=!this.showTeamChat
+  }
   }
 
   shouldShowTabsForUser(): boolean {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem('token'); // Retrieve token here
 
     if (token) {
-      const userHasPermission = false;
+      // Token is present, you can perform authorization checks here
+      // For demonstration purposes, let's assume userHasPermission is true
+      const userHasPermission = true; // Replace with your actual logic
       return userHasPermission;
     } else {
+      // Token is not present, user doesn't have permission
       return false;
     }
   }

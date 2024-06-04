e4b42c8e7ff077a55885e1a99a8e97294d1c3aa8
vikrant29k <vikrant.mobicloud@gmail.com>
2024-06-04 15:03:39 +0530

    image fixed and scroll to bottom added

--- src/app/pages/component/communications/team-chat/team-chat.component.ts
+++ src/app/pages/component/communications/team-chat/team-chat.component.ts
@@ -51,7 +51,6 @@ export class TeamChatComponent
   @ViewChild('videoInput') videoInput!: ElementRef;
   @ViewChild('documentInput') documentInput!: ElementRef;
   @ViewChild('chatwrapper') chatwrapper!: ElementRef;
-  // @ViewChild('videoPlayer', { static: true }) videoPlayer!: ElementRef;
   @ViewChild('videoPlayer') videoPlayer!: ElementRef<HTMLVideoElement>;
 
 
@@ -74,8 +73,6 @@ export class TeamChatComponent
     const response = await this.socket
       .timeout(5000)
       .emitWithAck('disconnected', { teamId: this.teamId });
-    // console.log(response, 'response of disconnect');
-    // this.socket.disconnect();
   }
   async ngOnInit() {
     // this.loadInitialChunk();
@@ -106,8 +103,8 @@ export class TeamChatComponent
             .timeout(5000)
             .emitWithAck('loadChat', {
               teamId: this.teamId,
-              limit: 25,
-              page: 1,
+              limit: 8,
+              page: this.page,
             });
           // console.log(response,"response of loadChat");
           if (response.status == 'error') {
@@ -199,18 +196,22 @@ export class TeamChatComponent
       // console.log(this.socket.id,"socket id");
       // compute a unique offset
       const clientOffset = `${this.socket.id}-${this.counter++}`;
-      this.chats.push(selfMessage);
+
       this.selectedImage = null;
       this.selectedVideo = null;
       this.selectedDocument = null;
       this.message = '';
-      this.scrollToBottom();
+      this.chats.push(selfMessage)
+        this.scrollToBottom();
+
       try {
         const response = await this.socket
           .timeout(5000)
           .emitWithAck('newTeamChat', data, clientOffset);
         // console.log(response,"response of newTeamChat"); // 'ok'
         if (response.status == 'ok') {
+
+
         }
         if (response.status == 'error') {
           alert(response.message);
@@ -223,49 +224,25 @@ export class TeamChatComponent
       //   this.teamChatTextarea.nativeElement.style.border = '2px solid red';
       // }
     }
-    // this.scrollToBottom();
   }
   @ViewChild('chatwrapper', { static: true }) chatWrapper!: ElementRef;
 
   scrollbarHeight!: number;
-  multiplyer=1;
+
   scrollToBottom() {
     try {
       console.log('scroll to bottom called');
-      this.scrollbarHeight = this.chatWrapper.nativeElement.scrollHeight+1000*this.multiplyer;
-      // console.log(this.chatWrapper.nativeElement); // Check if this logs the correct element
-      this.chatWrapper.nativeElement.scrollTop =
-        this.chatWrapper.nativeElement.scrollHeight;
+
+      // Wait for the DOM to update (consider using MutationObserver for more complex scenarios)
+      setTimeout(() => {
+        this.scrollbarHeight = this.chatWrapper.nativeElement.scrollHeight;
+        this.chatWrapper.nativeElement.scrollTop = this.scrollbarHeight;
+      }, 0);
+
     } catch (err) {
       console.error(err);
     }
   }
-  // scrollToBottom1() {
-  //   try {
-  //     // console.log("scroll height",this.chatWrapper.nativeElement.scrollHeight);
-  //     // console.log("setting scroll bar",this.chatWrapper.nativeElement.scrollHeight/this.counter1);
-
-  //     const newScrollHeight = this.chatWrapper.nativeElement.scrollHeight;
-  //     const addedContentHeight = newScrollHeight - this.previousScrollHeight;
-
-  //     console.log("Previous scroll height:", this.previousScrollHeight);
-  //   console.log("New scroll height:", newScrollHeight);
-  //   console.log("Added content height:", addedContentHeight);
-
-  //   this.chatWrapper.nativeElement.scrollTop += addedContentHeight;
-
-  //   console.log("Scroll position set to:", this.chatWrapper.nativeElement.scrollTop);
-
-  //   this.scrollbarHeight = this.chatWrapper.nativeElement.scrollHeight;
-  //   this.chatWrapper.nativeElement.scrollTop = this.scrollbarHeight;
-
-  //     // this.scrollbarHeight = this.chatWrapper.nativeElement.scrollHeight-this.scrollbarHeight
-  //     // this.chatWrapper.nativeElement.scrollTop =
-  //     //   this.chatWrapper.nativeElement.scrollHeight/this.counter1;
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // }
 
   toggleExpand(chat: any) {
     chat.expanded = !chat.expanded;
@@ -344,64 +321,9 @@ export class TeamChatComponent
     }
   }
 
-  chatsForPush = [
-    {
-      _id: '66599dd85dfe2c985db0378a',
-      senderId: {
-        superSuperUser: false,
-        _id: '665842d302198f56626128ec',
-        userName: 'ASD 2',
-        avatar: '1717060498275-images.png',
-        email: 'asd2@gmail.com',
-        password:
-          '$2b$10$inaSb9RWA2DAs0pK9Jej9uQ3/FsrR.0dGNTC/Rg7DMqCWipiRKvgG',
-        gameLeader: false,
-        teamId: '665842d302198f56626128e4',
-        companyUnit: 'Region East',
-        firstLogin: true,
-        targetOrSalesRepLC: 10000,
-        currentSalesRepLC: 100,
-        salesRepNo: 22,
-        role: 'user',
-        companyId: '66178631178345fda333c9cb',
-        superUser: false,
-        createdAt: '2024-05-30T09:11:47.164Z',
-        updatedAt: '2024-06-03T07:07:54.363Z',
-        __v: 0,
-        favoriteNewsFeed: {
-          '6658433e02198f5662612919': 'liked',
-          '66584d4f2fef1f71c09ce743': 'applouds',
-          '665d48f9db108dd788556667': 'rocket',
-        },
-      },
-      teamId: '665842d302198f56626128e4',
-      message: 'hi',
-      clientOffset: 'voREOGYlmPuoO5ouAAHz-0',
-      time: '2024-05-31T09:52:24.875Z',
-      __v: 0,
-    },
-  ];
   counter1 = 2;
   previousScrollHeight!: number;
 
-  // onScrollUp() {
-  //   console.log("scrolled up!!");
-  //   this.previousScrollHeight = this.chatWrapper.nativeElement.scrollHeight;
-  //   console.log("up height", this.previousScrollHeight);
-  //   this.chatsForPush.map((obj)=>{
-  //     this.chats.unshift(obj)
-
-  //   })
-
-  //   setTimeout(() => {
-  //     this.adjustScrollPosition();
-  //   }, 500);
-
-  //   // this.scrollToBottom1();
-  //   console.log(this.chats);
-  //   console.log("counter", this.counter1)
-  //   this.counter1++
-  // }
   async onScrollUp() {
     console.log('scrolled up!!');
     this.previousScrollHeight = this.chatWrapper.nativeElement.scrollHeight;
@@ -419,50 +341,45 @@ export class TeamChatComponent
     this.counter1++;
   }
 
+  page=1
   addChats(): Promise<void> {
-    return new Promise((resolve) => {
-      this.chatsForPush.map((obj) => {
-        this.chats.unshift(obj);
-      });
+    return new Promise( async (resolve) => {
+      this.page++
+      const response = await this.socket
+            .timeout(5000)
+            .emitWithAck('loadChat', {
+              teamId: this.teamId,
+              limit: 7,
+              page: this.page,
+            });
+          if (response.status == 'error') {
+           console.log(response.message);
+          }
+          if (response.status == 'ok') {
+            // const chats = response.existingChat;
+          this.chats = [...response.orderedChat,...this.chats];
+          console.log('chats', this.chats);
+          }
+
       resolve();
     });
   }
-
-  // adjustScrollPosition() {
-  //   try {
-  //     const newScrollHeight = this.chatWrapper.nativeElement.scrollHeight;
-  //     const addedContentHeight = newScrollHeight - this.previousScrollHeight;
-
-  //     console.log("Previous scroll height:", this.previousScrollHeight);
-  //     console.log("New scroll height:", newScrollHeight);
-  //     console.log("Added content height:", addedContentHeight);
-
-  //     this.chatWrapper.nativeElement.scrollTop += addedContentHeight;
-
-  //     console.log("Scroll position set to:", addedContentHeight);
-  //     this.chatWrapper.nativeElement.scrollTop = addedContentHeight;
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // }
+  multiplyer=1;
   adjustScrollPosition(): Promise<void> {
     return new Promise((resolve) => {
       setTimeout(() => {
         try {
           const newScrollHeight = this.chatWrapper.nativeElement.scrollHeight;
-          const addedContentHeight =
-            newScrollHeight - this.previousScrollHeight;
+          const addedContentHeight = newScrollHeight - this.previousScrollHeight;
 
           console.log('Previous scroll height:', this.previousScrollHeight);
           console.log('New scroll height:', newScrollHeight);
           console.log('Added content height:', addedContentHeight);
-
-          this.chatWrapper.nativeElement.scrollTop += addedContentHeight;
           console.log('Scroll position set to:', addedContentHeight);
 
           // Set the final scroll position
-          this.chatWrapper.nativeElement.scrollTop = addedContentHeight;
-
+          this.chatWrapper.nativeElement.scrollTop = addedContentHeight +500 * this.multiplyer;
+          this.multiplyer++
           resolve();
         } catch (err) {
           console.error(err);
@@ -471,20 +388,6 @@ export class TeamChatComponent
       }, 0); // Reduced timeout to ensure minimal delay
     });
   }
-
-  // @HostListener('scroll', ['$event'])
-  // onScroll(event: any) {
-  //   const scrollTop = this.chatwrapper.nativeElement.scrollTop;
-  //   console.log('scroll top', scrollTop);
-  //   // if (scrollTop === 0 && !this.loading) {
-  //   //   this.loadChats();
-  //   // }
-  //   if (scrollTop === 0) {
-  //   this.chatsForPush.map((obj)=>{
-  //     this.chats.unshift(obj)
-  //   })
-  // }
-  // }
 
   loadInitialChunk() {
     this.chatService.getVideoChunk().subscribe(
@@ -507,26 +410,8 @@ export class TeamChatComponent
       },
       (error) => {
         console.error('Error fetching video chunk:', error);
-      
-      // this.chatService.getVideoChunk('0').subscribe(blob => {
-      // const videoElement = this.videoPlayer.nativeElement;
-      // const url = URL.createObjectURL(blob);
-      // videoElement.src = url;
     });
   }
-
-  // onSeeking(event: Event) {
-  //   const videoElement = this.videoPlayer.nativeElement;
-  //   const currentTime = videoElement.currentTime;
-  //   const range = `bytes=${Math.floor(currentTime * 1000000)}-9999999`;
-
-  //   this.chatService.getVideoChunk(range).subscribe((blob) => {
-  //     const url = URL.createObjectURL(blob);
-  //     videoElement.src = url;
-  //     videoElement.currentTime = currentTime;
-  //     videoElement.play();
-  //   });
-  // }
 
   streamVideo(videoName:string){
 this.videoUrl = `${environment.baseUrl}chat/stream/${videoName}`

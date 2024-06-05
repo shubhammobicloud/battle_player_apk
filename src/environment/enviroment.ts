94ab52811cc7f20ced5bd8a3eecbe41feb159dc1
vikrant29k <vikrant.mobicloud@gmail.com>
2024-06-05 15:02:51 +0530

    minor uii change

--- src/environment/enviroment.ts
+++ src/environment/enviroment.ts
@@ -2,5 +2,5 @@ export const environment ={
   //  baseUrl:'http://89.116.122.208/'
   // baseUrl:'https://dev.mobicloudtechnologies.com/wuerth-backend/',
   // baseUrl:'http://192.168.29.28:3000/'
-  baseUrl:'http://192.168.29.27:3000/'
+  baseUrl:'http://192.168.29.28:3000/'
 }
